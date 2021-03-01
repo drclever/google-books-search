1cdc86e0d3afe6e4eec2092e3e850fe0a7e6694c
drclever <drc0625@aol.com>
2021-03-01 13:49:57 -0500

    Updated Navbar buttons text

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,15 +4,15 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-primary bg-info">
             <a className="navbar-brand" href="/">
-                <h2 className="text-white">Google Book Search</h2>
+                <h2 className="text-white">Google Books</h2>
             </a>
             <div id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item" id="home">
-                        <a className="nav-link" href="/"><button type="button" className="btn btn-primary text-white">Search Books</button></a>
+                        <a className="nav-link" href="/"><button type="button" className="btn btn-primary text-white">Search</button></a>
                     </li>
                     <li className="nav-item" id="report">
-                        <a className="nav-link" href="/saved"><button type="button" className="btn btn-primary text-white">Saved Books</button></a>
+                        <a className="nav-link" href="/saved"><button type="button" className="btn btn-primary text-white">Saved</button></a>
                     </li>
                 </ul>
             </div>
