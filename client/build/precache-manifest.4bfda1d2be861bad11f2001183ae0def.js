2921a3b8c5eb9c9a8d695a30868a76ad63619fa2
drclever <drc0625@aol.com>
2021-03-01 22:19:18 -0500

    Inital Load of build

---
+++ client/build/precache-manifest.4bfda1d2be861bad11f2001183ae0def.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cffe9db2888276808b22fa38ac62e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95e026ac82a1c2b562a9",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "f8f66866380351b088c9",
+    "url": "/static/css/main.75ec9f3f.chunk.css"
+  },
+  {
+    "revision": "95e026ac82a1c2b562a9",
+    "url": "/static/js/2.c65f989e.chunk.js"
+  },
+  {
+    "revision": "f2517fd5de2e31297dba10f163501a32",
+    "url": "/static/js/2.c65f989e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f66866380351b088c9",
+    "url": "/static/js/main.900cb3f5.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "d8631c5e3e343c2d962091d98add4e88",
+    "url": "/static/media/blur-book-stack-books-bookshelves-590493.d8631c5e.jpg"
+  }
+]);
